93ccf7b5ac9b00d12a880316fc247de30d50f3f6
iabru <75078144+iabru@users.noreply.github.com>
2023-04-18 20:45:07 +0200

    Uppdaterade historik färgerna
    Märkte att jag glömt att fixa så att historikfärgerna matchade tabsen. Nu är det fixat!

--- src/History/Icon.js
+++ src/History/Icon.js
@@ -1,13 +1,13 @@
 // How to start react: https://www.youtube.com/watch?v=Rs2BT6HrwMw
 import './Icon.css';
 
-function Icon(name, link, removeIcon) {
+function Icon(name, link, removeIcon, color) {
   return (
     <div className="chosenOption">
       <div className="Icon-cross" onClick={removeIcon}>
         <img src="https://drive.google.com/uc?export=view&id=13YujSwDcp0K8RCFAHvR0YGYNrpPB1Seb" alt="cross" />
       </div>
-      <img src={link} className="Icon-logo" alt={name} />
+      <img src={link} className="Icon-logo" id={color} alt={name} />
     </div>
   );
 }
--- src/History/IconNoCross.js
+++ src/History/IconNoCross.js
@@ -1,10 +1,10 @@
 // How to start react: https://www.youtube.com/watch?v=Rs2BT6HrwMw
 import './Icon.css';
 
-function IconNoCross(name, link) {
+function IconNoCross(name, link, color) {
   return (
     <div className="chosenOption">
-      <img src={link} className="Icon-logo" alt={name} />
+      <img src={link} className="Icon-logo" id={color} alt={name} />
     </div>
   );
 }
